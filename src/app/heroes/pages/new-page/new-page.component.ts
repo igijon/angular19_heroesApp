4107395ff58eac133eeb6c1b0220a76443f52fd8
Inma Gijón <igijoninf@gmail.com>
2025-02-13 17:55:15 +0100

    CRUD endpoints

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
-
+  public publishers = [
+    { id: 'DC Comics', desc: 'DC - Comics'},
+    { id: 'Marvel Comics', desc: 'Marvel - Comics'},
+  ]
 }
--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { environments } from '../../../environments/environments';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -27,5 +27,24 @@ export class HeroesService {
 
   getSuggestions(query: string): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes?q=${query}&_limit=6`);
+  }
+
+  addHero( hero: Hero ):Observable<Hero> {
+    return this.http.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero( hero: Hero ):Observable<Hero> {
+    //patch actualiza parcialmente el objeto
+    if(!hero.id) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.baseUrl}/heroes/${hero.id}`, hero);
+  }
+
+  deleteHeroById( id: string ):Observable<boolean> {
+    return this.http.delete(`${this.baseUrl}/heroes/${id}`)
+      .pipe(
+        catchError( err => of(false) ),
+        map( resp => true )
+      );
   }
 }
