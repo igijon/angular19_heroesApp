8246433e49b3be3de82572c09d29badaebbe34e5
Inma Gijón <igijoninf@gmail.com>
2025-02-18 10:36:51 +0100

    Mantener sesión del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -25,6 +25,17 @@ export class AuthService {
         tap( user => this.user = user ),
         tap( user => localStorage.setItem('token', 'dfadsfasdfn.ASDFASDf234234.ASDfasd34453'))
       );
+  }
+
+  checkAuthentication(): Observable<boolean> | boolean {
+    if(!localStorage.getItem('token')) return false;
+    const token = localStorage.getItem('token');
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ), //Doble negación que devuelve un valor booleano si existe
+        catchError( err => of(false))
+      )
   }
 
   logout() {
